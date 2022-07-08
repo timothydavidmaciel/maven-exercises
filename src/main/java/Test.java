13ddae0fe3a5c8d0bc78111471958acd3ea4c839
Timothydavid Maciel <timothydavidmaciel@gmail.com>
2022-07-08 15:14:24 -0500

    updating repo

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,14 +1,25 @@
-import org.apache.commons.collections4.MultiSet;
-import org.apache.commons.collections4.multiset.HashMultiSet;
+import java.util.Scanner;
+import org.apache.commons.lang3.StringUtils;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.Set;
 
 public class Test {
-    MultiSet<String> multiSet = new HashMultiSet<>();
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Well hello there, can you give me a string of words?");
+        String umNum = scanner.nextLine();
+        if (StringUtils.isNumeric(umNum)) {
+            System.out.println("We were looking for a string but got something else instead!");
+            System.exit(0);
+        } else {
+            System.out.println("\"" + umNum + "\" is not a number! yay!");
+        }
+
+        System.out.println("Let see the string case be flipped");
+        System.out.println(StringUtils.swapCase(umNum));
+
+        System.out.println("Now lets see that string in reverse!");
+        System.out.println(StringUtils.reverse(umNum));
+
 
     }
 }
